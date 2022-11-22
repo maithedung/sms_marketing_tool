97cfb31ce818a6292cf5720f0653a81dcd6827aa
Mai The Dung <44361757+maithedung@users.noreply.github.com>
2022-11-22 18:12:02 +0700

    update

--- sms_marketing.py
+++ sms_marketing.py
@@ -1,17 +1,26 @@
-#importing Twilio API
+# Importing Twilio API
 from twilio.rest import Client
-account_sid = "Your_Accoutn_Sid"
-auth_token  = "Your_Auth_Token"
+
+account_sid = "AC411362098e7ba95d3c69e9d27313e9a8"
+auth_token = "1836f1a95b821bb2f9e4983433b4eb96"
 client = Client(account_sid, auth_token)
 
-#Reading Massege From Text File  
-message = open("message.txt","r").read()
-contact = open("contact.txt", "r").read().split(',')
+message = client.messages.create(
+                              body='This is a Demo Text with twilio',
+                              from_='+19144605985',
+                              to='+84387275685'
+                          )
 
-#Looping All Number and send Messages
-for x in contact:
-    message = client.messages.create(
-        to=x, 
-        from_="+15028920419", #use your own Number From Twilio
-        body=message)
+print(message.sid)
+
+# # Reading Massege From Text File
+# message = open("message.txt", "r").read()
+# contact = open("contact.txt", "r").read().split(',')
+#
+# # Looping All Number and send Messages
+# for x in contact:
+#     message = client.messages.create(
+#         to=x,
+#         from_="+19144605985", # Use your own Number From Twilio
+#         body=message)
 
